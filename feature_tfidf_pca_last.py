f5dd17f10e9b9796502ec3f74c6aceb3d9fa436d
slowisfast2030 <slowisfast2030@163.com>
2022-08-30 15:30:05 +0800

    all is well

--- feature_tfidf_pca_last.py
+++ feature_tfidf_pca_last.py
@@ -172,11 +172,12 @@ if __name__ == "__main__":
     
     print("\n从文本列获取tfidf_pca向量\n")
     col_name_list = ['title', 'category_name', 'tags']
-    col_name_list = ['currentPosition', 'desiredPosition']
     col_name_list = ['jobTracks']
     col_name_list = ['projectTracks']
     col_name_list = ['tags']
     col_name_list = ['skills']
+    col_name_list = ['currentPosition', 'desiredPosition']
+
     get_tfidf_pca_from_text_cols(data_path, col_name_list, dimension=10)
 
     print("all is well")
